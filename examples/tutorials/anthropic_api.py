e637b9d941c7b786404cd3a7babf2c536abed0ff
Alfred Lua <alfredlua@users.noreply.github.com>
2025-03-19 17:06:57 +0800

    Add check for "native" reply tool

--- examples/tutorials/anthropic_api.py
+++ examples/tutorials/anthropic_api.py
@@ -12,9 +12,9 @@ import stores
 def main():
     # Load custom tools and set the required environment variables
     index = stores.Index(
-        ["./custom_tools"],
+        ["silanthro/send-gmail"],
         env_vars={
-            "./custom_tools": {
+            "silanthro/send-gmail": {
                 "GMAIL_ADDRESS": os.environ["GMAIL_ADDRESS"],
                 "GMAIL_PASSWORD": os.environ["GMAIL_PASSWORD"],
             },
@@ -23,7 +23,7 @@ def main():
 
     # Set up the user request, system instruction, model parameters, tools, and initial messages
     user_request = "Make up a parenting poem and email it to x@gmail.com"
-    system_instruction = "You are a helpful assistant who can generate poems in emails. When necessary, you have tools at your disposal. Always use the REPLY tool when you have completed the task. You do not have to ask for confirmations."
+    system_instruction = "You are a helpful assistant who can generate poems in emails. You do not have to ask for confirmations."
     model = "claude-3-5-sonnet-20241022"
     max_tokens = 1024
     tools = index.format_tools("anthropic")
@@ -43,38 +43,55 @@ def main():
             tools=tools,
         )
 
-        # Append the assistant's response as context
-        messages.append({"role": "assistant", "content": response.content})
+        # Check if all blocks contain only text, which indicates task completion for this example
+        blocks = response.content
+        if all(block.type == "text" for block in blocks):
+            print(f"Assistant response: {blocks[0].text}")
+            return # End the agent loop
 
-        # Process the response, which includes both text and tool use
-        for blocks in response.content:
-            if blocks.type == "text":
-                print(f"Assistant Response: {blocks.text}")
-            elif blocks.type == "tool_use":
-                print(f"Tool Call: {blocks}")
-                name = blocks.name.replace("-", ".")
-                args = blocks.input
+        # Otherwise, process the response, which includes both text and tool use
+        for block in blocks:
+            if block.type == "text":
+                print(f"Assistant response: {block.text}")
+                messages.append({"role": "assistant", "content": block.text})
+            elif block.type == "tool_use":
+                name = block.name.replace("-", ".")
+                args = block.input
 
                 # If the REPLY tool is called, break the loop and return the message
-                if blocks.name == "REPLY":
-                    print(f"Assistant Response: {blocks.input['msg']}")
-                    return
+                if name == "REPLY":
+                    print(f"Assistant response: {args['msg']}")
+                    return # End the agent loop
 
                 # Otherwise, execute the tool call
+                print(f"Executing tool call: {name}({args})")
                 output = index.execute(name, args)
+                print(f"Tool output: {output}")
                 messages.append(
                     {
-                        "role": "user",  # Some APIs require a tool role instead
+                        "role": "assistant",
+                        "content": [
+                            {
+                                "type": "tool_use",
+                                "id": block.id,
+                                "name": block.name, # Use the original block name or the API will return an error
+                                "input": block.input,
+                            }
+                        ],
+                    }
+                )
+                messages.append(
+                    {
+                        "role": "user",
                         "content": [
                             {
                                 "type": "tool_result",
-                                "tool_use_id": blocks.id,
+                                "tool_use_id": block.id,
                                 "content": str(output),
                             }
                         ],
                     }
                 )
-                print(f"Tool Output: {output}")
 
 
 if __name__ == "__main__":
